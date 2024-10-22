e8065226c3aec5cf5f9e127be957c16777a5527e
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2024-10-22 17:18:33 -0300

    Implemented SSL + Config

---
+++ FE-Backend-Project/src/main/java/com/example/febackendproject/Config/HttpsRedirectConfig.java
@@ -0,0 +1,24 @@
+package com.example.febackendproject.Config;
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpsRedirectConfig {
+    
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> redirectHttpToHttps() {
+        return factory -> factory.addAdditionalTomcatConnectors(httpConnector());
+    }
+    
+    private Connector httpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080); // Port for HTTP
+        connector.setSecure(false);
+        connector.setRedirectPort(8443); // Redirect to HTTPS port
+        return connector;
+    }
+}
