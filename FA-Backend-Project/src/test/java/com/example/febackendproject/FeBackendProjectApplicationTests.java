92fff0b3e88376cf2e7f68148626dce11155140b
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2024-10-26 20:10:03 -0300

    Rename Backend Project

---
+++ FA-Backend-Project/src/test/java/com/example/febackendproject/FeBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.febackendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeBackendProjectApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
