3737e486901001ecbfc33ad0c44747f47fd9c568
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2024-10-27 21:41:23 -0300

    Minor adjustment

--- FA-Frontend-Project/src/App.tsx
+++ FA-Frontend-Project/src/App.tsx
@@ -3,14 +3,9 @@ import "@/App.scss";
 import CustomRouter from "@/routes";
 import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 import { Welcome } from "@/Pages/Welcome/Welcome";
-import { useEffect } from "react";
 
 function App() {
   const { isAuthenticated } = useKindeAuth();
-
-  useEffect(() => {
-    console.log(isAuthenticated);
-  }, [isAuthenticated]);
 
   return (
     <div className="App">
