8bdb8bc89dcc9b48f808beba9d5d6096666e791d
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2024-10-30 15:43:08 -0300

    Minor Fix

--- FA-Frontend-Project/src/components/nav-user.tsx
+++ FA-Frontend-Project/src/components/nav-user.tsx
@@ -46,7 +46,6 @@ export function NavUser() {
                 <span className="truncate font-semibold">
                   {user?.given_name}
                 </span>
-                <span className="truncate text-xs">{user?.email}</span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
