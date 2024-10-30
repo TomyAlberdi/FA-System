d966a7093a7d1ede7efe0029b007e90c383ce8cc
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2024-10-30 18:53:27 -0300

    Implemented Desktop Individual Category and Provider Pages

--- FA-Frontend-Project/src/Pages/Categories/Category.tsx
+++ FA-Frontend-Project/src/Pages/Categories/Category.tsx
@@ -2,6 +2,24 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useCatalogContext } from "@/Context/UseCatalogContext";
 import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 interface Category {
   id: number;
@@ -14,43 +32,89 @@ const Category = () => {
 
   const { BASE_URL } = useCatalogContext();
 
-  const [Data, setData] = useState<Category | null>(null);
+  const [Category, setCategory] = useState<Category | null>(null);
   const [Loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchCategory = async () => {
       setLoading(true);
       try {
         const response = await fetch(`${BASE_URL}/category/${id}`);
         if (!response.ok) {
-          console.error("Error fetching data: ", response.statusText);
+          console.error("Error fetching Category: ", response.statusText);
           return;
         }
         const result: Category = await response.json();
-        setData(result);
+        setCategory(result);
       } catch (error) {
-        console.error("Error fetching data: ", error);
+        console.error("Error fetching Category: ", error);
       } finally {
         setLoading(false);
       }
     };
-    fetchData();
+    fetchCategory();
   }, [id, BASE_URL]);
 
   return (
     <div className="CatalogPage CategoryPage h-full">
-      {Loading ? (
+      {Loading || !Category ? (
         <div className="loading w-1/5 h-1/5">
           <h1 className="text-xl">Cargando...</h1>
           <Skeleton className="h-4 w-[100px]" />
         </div>
-      ) : Data ? (
+      ) : Category ? (
         <section className="CatalogPageData h-full w-full">
-          <h1>{Data.name}</h1>
+          <div className="CatalogPageInfo h-2/3 w-1/3">
+            <Card className="w-5/6">
+              <CardHeader>
+                <CardDescription className="text-xl">Categoría</CardDescription>
+                <CardTitle className="text-4xl">{Category.name}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <CardDescription className="text-lg">
+                  Cantidad de productos: {Category.productsAmount}
+                </CardDescription>
+              </CardContent>
+              <CardContent>
+                <Button className="w-full mb-2">
+                  Editar
+                </Button>
+                <Button variant="destructive" className="w-full">
+                  Eliminar
+                </Button>
+              </CardContent>
+            </Card>
+          </div>
+          <ScrollArea className="CatalogPageList w-2/3">
+            <h2 className="text-xl text-muted-foreground text-left pb-5">
+              Lista de productos
+            </h2>
+            <Table>
+              <TableCaption>Lista de productos</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-1/12">ID</TableHead>
+                  <TableHead className="w-1/3">Nombre</TableHead>
+                  <TableHead className="w-1/3">Stock</TableHead>
+                  <TableHead>Precio</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 5 }, (_, i) => {
+                  return (
+                    <TableRow key={i}>
+                      <TableCell className="font-medium">123</TableCell>
+                      <TableCell>Producto</TableCell>
+                      <TableCell>10 Cajas (100m2)</TableCell>
+                      <TableCell>$100</TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </ScrollArea>
         </section>
-      ) : (
-        <h1>Error</h1>
-      )}
+      ) : null}
     </div>
   );
 };
--- FA-Frontend-Project/src/Pages/Providers/Provider.tsx
+++ FA-Frontend-Project/src/Pages/Providers/Provider.tsx
@@ -1,7 +1,25 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useCatalogContext } from "@/Context/UseCatalogContext";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 interface Provider {
   id: number;
@@ -10,48 +28,89 @@ interface Provider {
 }
 
 export const Provider = () => {
-
   const { id } = useParams();
   const { BASE_URL } = useCatalogContext();
-  const [Data, setData] = useState<Provider | null>(null);
+  const [Provider, setProvider] = useState<Provider | null>(null);
   const [Loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchProvider = async () => {
       setLoading(true);
       try {
         const response = await fetch(`${BASE_URL}/provider/${id}`);
         if (!response.ok) {
-          console.error("Error fetching data: ", response.statusText);
+          console.error("Error fetching Provider: ", response.statusText);
           return;
         }
         const result: Provider = await response.json();
-        setData(result);
+        setProvider(result);
       } catch (error) {
-        console.error("Error fetching data: ", error);
+        console.error("Error fetching Provider: ", error);
       } finally {
         setLoading(false);
       }
     };
-    fetchData();
+    fetchProvider();
   }, [id, BASE_URL]);
 
   return (
     <div className="CatalogPage ProviderPage h-full">
-      {
-        Loading ? (
-          <div className="loading w-1/5 h-1/5">
-            <h1 className="text-xl">Cargando...</h1>
-            <Skeleton className="h-4 w-[100px]" />
+      {Loading || !Provider ? (
+        <div className="loading w-1/5 h-1/5">
+          <h1 className="text-xl">Cargando...</h1>
+          <Skeleton className="h-4 w-[100px]" />
+        </div>
+      ) : Provider ? (
+        <section className="CatalogPageData h-full w-full">
+          <div className="CatalogPageInfo h-2/3 w-1/3">
+            <Card className="w-5/6">
+              <CardHeader>
+                <CardDescription className="text-xl">Proveedor</CardDescription>
+                <CardTitle className="text-4xl">{Provider.name}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <CardDescription className="text-lg">
+                  Cantidad de productos: {Provider.productsAmount}
+                </CardDescription>
+              </CardContent>
+              <CardContent>
+                <Button className="w-full mb-2">Editar</Button>
+                <Button variant="destructive" className="w-full">
+                  Eliminar
+                </Button>
+              </CardContent>
+            </Card>
           </div>
-        ) : Data ? (
-          <section className="CatalogPageData h-full w-full">
-            <h1>{Data.name}</h1>
-          </section>
-        ) : (
-          <h1>Error</h1>
-        )
-      }
+          <ScrollArea className="CatalogPageList w-2/3">
+            <h1 className="text-xl text-muted-foreground text-left pb-5">
+              Lista de productos
+            </h1>
+            <Table>
+              <TableCaption>Lista de productos</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-1/12">ID</TableHead>
+                  <TableHead className="w-1/3">Nombre</TableHead>
+                  <TableHead className="w-1/3">Stock</TableHead>
+                  <TableHead>Precio</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 5 }, (_, i) => {
+                  return (
+                    <TableRow key={i}>
+                      <TableCell className="font-medium">123</TableCell>
+                      <TableCell>Producto</TableCell>
+                      <TableCell>10 Cajas (100m2)</TableCell>
+                      <TableCell>$100</TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </ScrollArea>
+        </section>
+      ) : null}
     </div>
-  )
-}
+  );
+};
