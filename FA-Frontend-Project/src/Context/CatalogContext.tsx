373f1a702abaeca94854340dd3a5568557e89e87
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2024-11-01 18:17:11 -0300

    Minor fixes

--- FA-Frontend-Project/src/Context/CatalogContext.tsx
+++ FA-Frontend-Project/src/Context/CatalogContext.tsx
@@ -1,7 +1,21 @@
 import { createContext } from 'react';
 
+interface Category {
+  id: number;
+  name: string;
+  productsAmount: number;
+}
+
+interface Provider {
+  id: number;
+  name: string;
+  productsAmount: number;
+}
+
 export interface CatalogContextType {
   BASE_URL: string;
+  fetchCategories: () => Promise<Array<Category> | undefined>;
+  fetchProviders: () => Promise<Array<Provider> | undefined>;
 }
 
 export const CatalogContext = createContext<CatalogContextType | null>(null);
