e60a606b4119c50285c7405adea33b2405a01f52
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-11-03 03:04:03 -0300

    Started add product form

--- FA-Frontend-Project/src/Pages/Products/ProductPagination.tsx
+++ FA-Frontend-Project/src/Pages/Products/ProductPagination.tsx
@@ -1,9 +1,13 @@
 import { ProductsHeader } from "@/Pages/Products/ProductsHeader";
+import { useState } from "react";
 
 export const ProductPagination = () => {
+
+  const [UpdateData, setUpdateData] = useState(false);
+
   return (
     <section className="ProductPagination col-span-6">
-      <ProductsHeader />
+      <ProductsHeader setUpdateData={setUpdateData} UpdateData={UpdateData} />
     </section>
   );
 };
