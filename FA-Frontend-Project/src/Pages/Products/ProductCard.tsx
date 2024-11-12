c6569d584e31240927e025e3ec9211e790d449e3
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2024-11-12 20:03:58 -0300

    Minor fixes

--- FA-Frontend-Project/src/Pages/Products/ProductCard.tsx
+++ FA-Frontend-Project/src/Pages/Products/ProductCard.tsx
@@ -1,10 +1,7 @@
 import { CardProduct } from "@/hooks/CatalogInterfaces";
 import {
   Card,
-  CardContent,
   CardDescription,
-  CardFooter,
-  CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
