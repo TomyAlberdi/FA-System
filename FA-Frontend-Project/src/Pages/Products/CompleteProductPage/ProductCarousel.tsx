fbe58663c6134d1975bf71a4b3bf1d36a4b4936e
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2024-11-18 20:07:52 -0300

    Implemented Product page image carousel

--- FA-Frontend-Project/src/Pages/Products/CompleteProductPage/ProductCarousel.tsx
+++ FA-Frontend-Project/src/Pages/Products/CompleteProductPage/ProductCarousel.tsx
@@ -1,33 +1,41 @@
-import { Card, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
+  CarouselItem,
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { Skeleton } from "@/components/ui/skeleton";
 import { CompleteProduct } from "@/hooks/CatalogInterfaces";
 
-export const ProductCarousel = ({ Product }: { Product: CompleteProduct | null }) => {
+export const ProductCarousel = ({
+  Product,
+}: {
+  Product: CompleteProduct | null;
+}) => {
   return (
-    <Carousel className="imageCarousel row-start-1 row-end-7 col-start-1 col-end-5 productGridItem">
-      <CarouselContent>
-        {Product?.images && Product?.images.length > 0 ? (
-          Product.images.map((image: string, i: number) => {
-            return (
-              <Card key={i} className="p-2">
-                <CardContent className="flex aspect-square items-center justify-center">
-                  <img src={image} alt={`Imagen ${i} del producto.`} />
-                </CardContent>
-              </Card>
-            );
-          })
-        ) : (
-          <Skeleton className="w-full h-full" />
-        )}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <div className="row-start-1 row-end-7 col-start-1 col-end-5 productGridItem flex justify-center items-center">
+      <Carousel className="imageCarousel w-full h-full flex justify-center items-center">
+        <CarouselContent className="w-full h-full">
+          {Product?.images && Product?.images.length > 0 ? (
+            Product.images.map((image: string, i: number) => {
+              return (
+                <CarouselItem key={i} className="w-full h-full">
+                  <Card
+                    className="w-full h-full bg-contain bg-no-repeat bg-center"
+                    style={{ backgroundImage: `url(${image})` }}
+                  ></Card>
+                </CarouselItem>
+              );
+            })
+          ) : (
+            <Skeleton className="w-full h-full" />
+          )}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
   );
 };
