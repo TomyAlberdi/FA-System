434a2f7a522fc667d2e3e58c6e395d6850d17986
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2024-11-19 19:18:27 -0300

    Implemented Price Accordion

--- FA-Frontend-Project/src/Pages/Products/CompleteProductPage/ProductComplementaryInfo.tsx
+++ FA-Frontend-Project/src/Pages/Products/CompleteProductPage/ProductComplementaryInfo.tsx
@@ -1,3 +1,9 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { CompleteProduct } from "@/hooks/CatalogInterfaces";
 
 export const ProductComplementaryInfo = ({
@@ -6,8 +12,48 @@ export const ProductComplementaryInfo = ({
   Product: CompleteProduct | null;
 }) => {
   return (
-    <div className="complementaryInfo row-start-5 row-end-16 col-start-5 col-end-16 productGridItem">
-      complementary info
+    <div className="complementaryInfo row-start-5 row-end-16 col-start-5 col-end-16 productGridItem px-2 py-4">
+      <Accordion type="multiple" className="w-full">
+        <AccordionItem value="price">
+          <AccordionTrigger>Precio</AccordionTrigger>
+          <AccordionContent className="px-2 flex flex-col justify-start items-start text-lg gap-2">
+            {Product?.discountPercentage && Product?.discountPercentage > 0 ? (
+              <>
+                <span className="p-2 bg-destructive text-destructive-foreground rounded-md">
+                  EN OFERTA -{Product?.discountPercentage}% :
+                </span>
+                <span>
+                  $ {Product?.discountedPrice} x {Product?.saleUnit}
+                </span>
+                {Product?.saleUnit !== Product?.measureType && (
+                  <span>
+                    $ {Product?.discountedMeasurePrice} x {Product?.measureType}
+                  </span>
+                )}
+              </>
+            ) : (
+              <>
+                <span>
+                  $ {Product?.saleUnitPrice} x {Product?.saleUnit}
+                </span>
+                {Product?.saleUnit !== Product?.measureType && (
+                  <span>
+                    $ {Product?.measurePrice} x {Product?.measureType}
+                  </span>
+                )}
+              </>
+            )}
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="stock">
+          <AccordionTrigger>Stock</AccordionTrigger>
+          <AccordionContent>Info stock</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="characteristics">
+          <AccordionTrigger>Características</AccordionTrigger>
+          <AccordionContent>Info características</AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 };
