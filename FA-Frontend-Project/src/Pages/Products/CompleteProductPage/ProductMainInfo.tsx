6e60a91a5fdff72b6f31c2ee770469fa5eeb2b6a
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2024-11-23 02:41:08 -0300

    Added product ID tag + unlisted product tag

--- FA-Frontend-Project/src/Pages/Products/CompleteProductPage/ProductMainInfo.tsx
+++ FA-Frontend-Project/src/Pages/Products/CompleteProductPage/ProductMainInfo.tsx
@@ -10,7 +10,7 @@ export const ProductMainInfo = ({
   Product: CompleteProduct | null;
 }) => {
   return (
-    <Card className="p-2 flex flex-col justify-start items-start row-start-1 row-end-5 col-start-5 col-end-16 productGridItem">
+    <Card className="p-2 flex flex-col justify-start items-start row-start-1 row-end-5 col-start-5 col-end-16 productGridItem relative">
       <div className="categoryLinks flex flex-row justify-start items-center gap-2 font-semibold">
         <Button variant={"ghost"}>
           <Link to={`/catalog/categories/${Product?.categoryId}`}>
@@ -37,9 +37,15 @@ export const ProductMainInfo = ({
           {Product?.provider}
         </Link>
       </Button>
-      <p className="px-4">
-        {Product?.description}
-      </p>
+      <p className="px-4">{Product?.description}</p>
+      <span className="absolute right-0 top-2 px-4 py-1 bg-chart-1 rounded-l-md text-lg font-semibold text-white">
+        ID {Product?.id}
+      </span>
+      {Product?.disabled && (
+        <span className="absolute right-0 top-14 px-4 py-1 bg-destructive rounded-l-md text-lg font-semibold text-white">
+          DESACTIVADO
+        </span>
+      )}
     </Card>
   );
 };
