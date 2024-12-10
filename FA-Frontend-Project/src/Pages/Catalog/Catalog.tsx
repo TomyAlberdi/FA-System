2311aaccbd9e6a74438630685ac61c6971d37876
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2024-12-10 16:54:50 -0300

    Started get last stock records chart impl

--- FA-Frontend-Project/src/Pages/Catalog/Catalog.tsx
+++ FA-Frontend-Project/src/Pages/Catalog/Catalog.tsx
@@ -1,11 +1,13 @@
 import { ProductsByCategory } from "@/Pages/Catalog/charts/ProductsByCategory"
 import { ProductsByProvider } from "@/Pages/Catalog/charts/ProductsByProvider"
+import { LastStockRecords } from "@/Pages/Catalog/charts/LastStockRecords"
 
 export const Catalog = () => {
   return (
     <div className="Catalog ProductPage h-full w-full">
       <ProductsByCategory />
       <ProductsByProvider />
+      <LastStockRecords />
     </div>
   )
 }
---
+++ FA-Frontend-Project/src/Pages/Catalog/charts/LastStockRecords.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/ui/card"
+
+export const LastStockRecords = () => {
+  return (
+    <Card className="col-start-9 col-span-7 row-span-7">
+
+    </Card>
+  )
+}
