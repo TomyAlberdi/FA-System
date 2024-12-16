8ffb10c578ad6132e001a2dd7889bf5bc7d5e356
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2024-12-16 17:39:19 -0300

    Added placeholder home page

--- FA-Frontend-Project/src/Pages/Home/Home.tsx
+++ FA-Frontend-Project/src/Pages/Home/Home.tsx
@@ -1,6 +1,16 @@
+import { Activity } from "lucide-react";
+
 const Home = () => {
   return (
-    <div className="Home">Home</div>
-  )
-}
-export default Home
+    <div className="Home h-full w-full flex flex-row items-center justify-center">
+      <div className="flex aspect-square size-28 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+        <Activity className="size-20" />
+      </div>
+      <div className="text-3xl leading-tight ml-4 flex flex-col items-left justify-center">
+        <span className="truncate font-semibold">F&A S.A.</span>
+        <span>Administración</span>
+      </div>
+    </div>
+  );
+};
+export default Home;
