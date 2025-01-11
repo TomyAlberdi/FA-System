b2f1378b0cc003cbe17cb6c260029994e43087f2
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-01-10 22:48:51 -0300

    Created Client entity

---
+++ FA-Backend-Project/src/main/java/com/example/febackendproject/Entity/Client.java
@@ -0,0 +1,27 @@
+package com.example.febackendproject.Entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @NotBlank
+    private String name;
+    
+    
+    
+}
