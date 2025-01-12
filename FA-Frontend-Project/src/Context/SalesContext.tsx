3334022caed362b737cb8acc003e8fee9c2d8524
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-01-11 21:10:37 -0300

    Added client data state & fetch functions

--- FA-Frontend-Project/src/Context/SalesContext.tsx
+++ FA-Frontend-Project/src/Context/SalesContext.tsx
@@ -1,7 +1,13 @@
+import { ReturnData } from "@/hooks/CatalogInterfaces";
+import { CompleteClient } from "@/hooks/SalesInterfaces";
 import { createContext } from "react";
 
 export interface SalesContextType {
   BASE_URL: string;
+  // Clients
+  Clients: ReturnData;
+  fetchClients: () => Promise<void>;
+  fetchClient: (id: number) => Promise<CompleteClient | undefined>;
 }
 
 export const SalesContext = createContext<SalesContextType | null>(null);
--- FA-Frontend-Project/src/Context/SalesContextComponent.tsx
+++ FA-Frontend-Project/src/Context/SalesContextComponent.tsx
@@ -1,26 +1,96 @@
 import { SalesContext, SalesContextType } from "@/Context/SalesContext";
+import { ReturnData } from "@/hooks/CatalogInterfaces";
+import { CompleteClient, PartialClient } from "@/hooks/SalesInterfaces";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+import { useEffect, useState } from "react";
 
 interface SalesContextComponentProps {
   children: React.ReactNode;
 }
 
-const SalesContextComponent: React.FC<SalesContextComponentProps> = ({ children }) => {
-  //const { toast } = useToast();
-  //const { getToken } = useKindeAuth();
+const SalesContextComponent: React.FC<SalesContextComponentProps> = ({
+  children,
+}) => {
+  const { getToken } = useKindeAuth();
   const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-  
+  useEffect(() => {
+    const fetchData = async () => {
+      if (getToken) {
+        const accessToken = await getToken();
+        if (accessToken) {
+          fetchClients();
+        }
+      }
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [getToken]);
+
+  /// CLIENT GET ///
+
+  const [Clients, setClients] = useState<ReturnData>({
+    Loading: true,
+    data: Array<PartialClient>(),
+  });
+
+  const fetchClients = async () => {
+    try {
+      if (!getToken) {
+        console.error("getToken is undefined");
+        return;
+      }
+      const accessToken = await getToken();
+      const response = await fetch(`${BASE_URL}/clients`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      if (!response.ok) {
+        console.error("Error fetching data: ", response.statusText);
+        return;
+      }
+      const result: Array<PartialClient> = await response.json();
+      setClients({ Loading: false, data: result });
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+      setClients({ Loading: false, data: [] });
+    }
+  };
+
+  const fetchClient = async (id: number) => {
+    try {
+      if (!getToken) {
+        console.error("getToken is undefined");
+        return;
+      }
+      const accessToken = await getToken();
+      const response = await fetch(`${BASE_URL}/clients/${id}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      if (!response.ok) {
+        console.error("Error fetching data: ", response.statusText);
+        return;
+      }
+      const result: CompleteClient = await response.json();
+      return result;
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
 
   const exportData: SalesContextType = {
     BASE_URL,
+    Clients,
+    fetchClients,
+    fetchClient,
   };
 
   return (
-    <SalesContext.Provider value={exportData}>
-      {children}
-    </SalesContext.Provider>
+    <SalesContext.Provider value={exportData}>{children}</SalesContext.Provider>
   );
-
-}
+};
 
 export default SalesContextComponent;
