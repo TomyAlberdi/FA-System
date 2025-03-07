8ed9158409f2646acc5d3a9896eebddcc3c839d4
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-03-07 19:47:48 -0300

    Updated BasicDataTab structure

--- FA-Frontend-Project/src/Pages/Products/CreateProduct/BasicDataTab.tsx
+++ FA-Frontend-Project/src/Pages/Products/CreateProduct/BasicDataTab.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectContent,
@@ -63,107 +64,131 @@ const BasicDataTab = ({ onNext, Product, setProduct }: BasicDataTabProps) => {
   ]);
 
   return (
-    <TabsContent
-      value="basicData"
-      className="h-full w-full grid grid-cols-4 grid-rows-9 gap-2"
-    >
-      <Input
-        placeholder="Nombre del producto"
-        className="row-start-1 col-span-2 h-full"
-        onChange={(e) =>
-          setProduct((prev) => ({ ...prev, name: e.target.value }))
-        }
-      />
-      <Input
-        placeholder="Código"
-        className="row-start-1 col-start-3 h-full"
-        type="number"
-        onChange={(e) =>
-          setProduct((prev) => ({ ...prev, code: e.target.value }))
-        }
-      />
-      <Input
-        placeholder="Calidad (Opcional)"
-        className="row-start-1 col-start-4 h-full"
-        onChange={(e) =>
-          setProduct((prev) => ({ ...prev, quality: e.target.value }))
-        }
-      />
-      <Textarea
-        placeholder="Descripción"
-        className="row-start-2 row-end-6 col-span-4"
-        onChange={(e) =>
-          setProduct((prev) => ({ ...prev, description: e.target.value }))
-        }
-      />
-      <Select
-        disabled={Providers?.Loading}
-        onValueChange={(value) =>
-          setProduct((prev) => ({ ...prev, providerId: Number(value) }))
-        }
-      >
-        <SelectTrigger className="row-start-6 col-span-4 h-full">
-          <SelectValue placeholder="Proveedor" />
-        </SelectTrigger>
-        <SelectContent>
-          {Array.isArray(Providers?.data) &&
-            (Providers?.data as Provider[]).map(
-              (provider: Provider, index: number) => {
+    <TabsContent value="basicData" className="h-full w-full">
+      <div className="h-full w-full grid grid-cols-4 grid-rows-9 gap-2">
+        <div className="row-start-1 col-span-2 h-full">
+          <Label className="text-md">Nombre</Label>
+          <Input
+            value={Product?.name}
+            onChange={(e) =>
+              setProduct((prev) => ({ ...prev, name: e.target.value }))
+            }
+          />
+        </div>
+        <div className="row-start-1 col-start-3 h-full">
+          <Label className="text-md">Código</Label>
+          <Input
+            value={Product?.code}
+            min={0}
+            type="number"
+            onChange={(e) =>
+              setProduct((prev) => ({ ...prev, code: e.target.value }))
+            }
+          />
+        </div>
+        <div className="row-start-1 col-start-4 h-full">
+          <Label className="text-md">Calidad</Label>
+          <Input
+            value={Product?.quality}
+            onChange={(e) =>
+              setProduct((prev) => ({ ...prev, quality: e.target.value }))
+            }
+          />
+        </div>
+        <div className="row-start-2 row-end-6 col-span-4">
+          <Label className="text-md">Descripción</Label>
+          <Textarea
+            value={Product?.description}
+            className="h-[90%]"
+            onChange={(e) =>
+              setProduct((prev) => ({ ...prev, description: e.target.value }))
+            }
+          />
+        </div>
+        <div className="row-start-6 col-span-4 h-full">
+          <Label className="text-md">Proveedor</Label>
+          <Select
+            disabled={Providers?.Loading}
+            value={Product?.providerId.toString()}
+            onValueChange={(value) =>
+              setProduct((prev) => ({ ...prev, providerId: Number(value) }))
+            }
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Proveedor" />
+            </SelectTrigger>
+            <SelectContent>
+              {Array.isArray(Providers?.data) &&
+                (Providers?.data as Provider[]).map(
+                  (provider: Provider, index: number) => {
+                    return (
+                      <SelectItem value={provider.id.toString()} key={index} className="cursor-pointer">
+                        {provider.name}
+                      </SelectItem>
+                    );
+                  }
+                )}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="row-start-7 col-span-4 h-full">
+          <Label className="text-md">Categoría</Label>
+          <Select
+            disabled={Categories?.Loading}
+            value={Product?.categoryId.toString()}
+            onValueChange={(value) =>
+              setProduct((prev) => ({ ...prev, categoryId: Number(value) }))
+            }
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Categoría" />
+            </SelectTrigger>
+            <SelectContent>
+              {Array.isArray(Categories?.data) &&
+                (Categories?.data as Category[]).map(
+                  (category: Category, index: number) => {
+                    return (
+                      <SelectItem value={category.id.toString()} key={index} className="cursor-pointer">
+                        {category.name}
+                      </SelectItem>
+                    );
+                  }
+                )}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="row-start-8 col-span-4 h-full">
+          <Label className="text-md">Subcategoría</Label>
+          <Select
+            disabled={Subcategories?.length === 0}
+            value={Product?.subcategoryId.toString()}
+            onValueChange={(value) =>
+              setProduct((prev) => ({ ...prev, subcategoryId: Number(value) }))
+            }
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Subcategoría" />
+            </SelectTrigger>
+            <SelectContent>
+              {Subcategories?.map((subcategory: Subcategory, index: number) => {
                 return (
-                  <SelectItem value={provider.id.toString()} key={index}>
-                    {provider.name}
+                  <SelectItem value={subcategory.id.toString()} key={index} className="cursor-pointer">
+                    {subcategory.name}
                   </SelectItem>
                 );
-              }
-            )}
-        </SelectContent>
-      </Select>
-      <Select
-        disabled={Categories?.Loading}
-        onValueChange={(value) =>
-          setProduct((prev) => ({ ...prev, categoryId: Number(value) }))
-        }
-      >
-        <SelectTrigger className="row-start-7 col-span-4 h-full">
-          <SelectValue placeholder="Categoría" />
-        </SelectTrigger>
-        <SelectContent>
-          {Array.isArray(Categories?.data) &&
-            (Categories?.data as Category[]).map(
-              (category: Category, index: number) => {
-                return (
-                  <SelectItem value={category.id.toString()} key={index}>
-                    {category.name}
-                  </SelectItem>
-                );
-              }
-            )}
-        </SelectContent>
-      </Select>
-      <Select
-        disabled={Subcategories?.length === 0}
-        onValueChange={(value) =>
-          setProduct((prev) => ({ ...prev, subcategoryId: Number(value) }))
-        }
-      >
-        <SelectTrigger className="row-start-8 col-span-4 h-full">
-          <SelectValue placeholder="Subcategoría" />
-        </SelectTrigger>
-        <SelectContent>
-          {Subcategories?.map((subcategory: Subcategory, index: number) => {
-            return (
-              <SelectItem value={subcategory.id.toString()} key={index}>
-                {subcategory.name}
-              </SelectItem>
-            );
-          })}
-        </SelectContent>
-      </Select>
-      <div className="row-start-9 col-span-4 flex flex-row justify-center items-center">
-        <Button onClick={onNext} className="gap-2 w-1/4" disabled={DisableNext}>
-          Siguiente
-          <ChevronRight size={16} />
-        </Button>
+              })}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="row-start-9 col-span-4 flex flex-row justify-center items-center">
+          <Button
+            onClick={onNext}
+            className="gap-2 w-1/4" /* disabled={DisableNext} */
+          >
+            Siguiente
+            <ChevronRight size={16} />
+          </Button>
+        </div>
       </div>
     </TabsContent>
   );
