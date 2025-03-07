06d30059fcbb43a79c4383bfa3bb38360c650208
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-03-07 19:48:20 -0300

    Updated CreateProduct to allow prop default value (Update Product)

--- FA-Frontend-Project/src/Pages/Products/CreateProduct/CreateProduct.tsx
+++ FA-Frontend-Project/src/Pages/Products/CreateProduct/CreateProduct.tsx
@@ -14,37 +14,39 @@ import ExtraDataTab from "@/Pages/Products/CreateProduct/ExtraDataTab";
 import { CreateProductDTO } from "@/hooks/CatalogInterfaces";
 import { Progress } from "@/components/ui/progress";
 
-const CreateProduct = () => {
+const CreateProduct = ({ ProductProp }: { ProductProp?: CreateProductDTO }) => {
   const [Product, setProduct] = useState<CreateProductDTO>({
-    name: "",
-    description: "",
-    quality: "",
-    code: "",
-    measureType: "M2",
-    measures: "",
-    saleUnit: "Caja",
-    saleUnitPrice: 0,
-    saleUnitCost: 0,
-    measurePerSaleUnit: 0,
-    discountPercentage: 0,
-    providerId: 0,
-    categoryId: 0,
-    subcategoryId: 0,
-    images: [],
-    color: "",
-    origen: "",
-    borde: "",
-    aspecto: "",
-    textura: "",
-    transito: "",
+    // Tab 1
+    name: ProductProp?.name ?? "",
+    description: ProductProp?.description ?? "",
+    quality: ProductProp?.quality ?? "",
+    code: ProductProp?.code ?? "",
+    providerId: ProductProp?.providerId ?? 0,
+    categoryId: ProductProp?.categoryId ?? 0,
+    subcategoryId: ProductProp?.subcategoryId ?? 0,
+    // Tab 2
+    measureType: ProductProp?.measureType ?? "M2",
+    measures: ProductProp?.measures ?? "",
+    saleUnit: ProductProp?.saleUnit ?? "Caja",
+    saleUnitPrice: ProductProp?.saleUnitPrice ?? 0,
+    saleUnitCost: ProductProp?.saleUnitCost ?? 0,
+    measurePerSaleUnit: ProductProp?.measurePerSaleUnit ?? 0,
+    discountPercentage: ProductProp?.discountPercentage ?? 0,
+    // Tab 3
+    images: ProductProp?.images ?? [],
+    color: ProductProp?.color ?? "",
+    origen: ProductProp?.origen ?? "",
+    borde: ProductProp?.borde ?? "",
+    aspecto: ProductProp?.aspecto ?? "",
+    textura: ProductProp?.textura ?? "",
+    transito: ProductProp?.transito ?? "",
   });
 
   const [DialogOpen, setDialogOpen] = useState(false);
 
   // Tabs management
   const [currentTab, setCurrentTab] = useState("basicData");
-  const tabs = ["basicData", "saleData", "extraData"];
-  const [progress, setProgress] = useState(33)
+  const [progress, setProgress] = useState(33);
 
   useEffect(() => {
     switch (currentTab) {
@@ -58,13 +60,27 @@ const CreateProduct = () => {
         setProgress(100);
         break;
     }
-  }, [currentTab])
-  
+  }, [currentTab]);
 
   const handleNextTab = () => {
-    const currentIndex = tabs.indexOf(currentTab);
-    if (currentIndex < tabs.length - 1) {
-      setCurrentTab(tabs[currentIndex + 1]);
+    switch (currentTab) {
+      case "basicData":
+        setCurrentTab("saleData");
+        break;
+      case "saleData":
+        setCurrentTab("extraData");
+        break;
+    }
+  };
+
+  const handlePreviousTab = () => {
+    switch (currentTab) {
+      case "saleData":
+        setCurrentTab("basicData");
+        break;
+      case "extraData":
+        setCurrentTab("saleData");
+        break;
     }
   };
 
@@ -82,7 +98,9 @@ const CreateProduct = () => {
         className="lg:w-[70vw] xl:max-w-[1344px] h-[80vh] flex flex-col justify-start "
       >
         <div className="flex flex-row items-center">
-          <DialogTitle className="text-3xl font-bold">Crear Producto</DialogTitle>
+          <DialogTitle className="text-3xl font-bold">
+            Crear Producto
+          </DialogTitle>
           <Progress value={progress} max={100} className="w-[50%] ml-[3%]" />
         </div>
         <Tabs
@@ -95,7 +113,11 @@ const CreateProduct = () => {
             Product={Product}
             setProduct={setProduct}
           />
-          <SaleDataTab onNext={handleNextTab} />
+          <SaleDataTab
+            onNext={handleNextTab}
+            Product={Product}
+            setProduct={setProduct}
+          />
           <ExtraDataTab onNext={handleNextTab} />
         </Tabs>
       </DialogContent>
