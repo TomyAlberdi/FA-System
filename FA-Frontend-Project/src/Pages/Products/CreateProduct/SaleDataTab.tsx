862d2a180a0bad8fc25cf50bd2d8eb56c7edd252
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-03-08 18:17:25 -0300

    Implemented SaleDataTab

--- FA-Frontend-Project/src/Pages/Products/CreateProduct/SaleDataTab.tsx
+++ FA-Frontend-Project/src/Pages/Products/CreateProduct/SaleDataTab.tsx
@@ -34,22 +34,41 @@ const SaleDataTab = ({
   Product,
   setProduct,
 }: SaleDataTabProps) => {
-  // Calculate the sale price
+  // Calculate profit margin if product has sale unit price and sale unit cost
   const [Rentabilidad, setRentabilidad] = useState(0);
+  useEffect(() => {
+    if (Product?.saleUnitPrice && Product?.saleUnitCost) {
+      const profitMargin =
+        ((Product?.saleUnitPrice - Product?.saleUnitCost) /
+          Product?.saleUnitCost) *
+        100;
+      setRentabilidad(Math.round(profitMargin * 100) / 100);
+    } else {
+      setRentabilidad(0);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  // Calculate and set sale unit price if product has sale unit cost and profit margin
   const getSalePrice = (profitability: number, saleUnitCost: number) => {
     const markup = profitability / 100;
     const salePrice = saleUnitCost * (1 + markup);
     return Math.round(salePrice * 100) / 100;
   };
   useEffect(() => {
-    setProduct((prev) => ({
-      ...prev,
-      saleUnitPrice: getSalePrice(Rentabilidad, Product?.saleUnitCost),
-    }));
+    if (Rentabilidad > 0) {
+      setProduct((prev) => ({
+        ...prev,
+        saleUnitPrice: getSalePrice(Rentabilidad, Product?.saleUnitCost),
+      }));
+    } else {
+      setProduct((prev) => ({
+        ...prev,
+        saleUnitPrice: Product?.saleUnitCost,
+      }));
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [Rentabilidad, Product?.saleUnitCost]);
-
-  // Calculate the measure price
+  // Calculate and set measure price if product has measure per sale unit and sale unit price
   const [MeasurePrice, setMeasurePrice] = useState(0);
   const getMeasurePrice = (
     measurePerSaleUnit: number,
@@ -59,9 +78,11 @@ const SaleDataTab = ({
     return Math.round(measurePrice);
   };
   useEffect(() => {
-    setMeasurePrice(
-      getMeasurePrice(Product?.measurePerSaleUnit, Product?.saleUnitPrice)
-    );
+    if (Product?.saleUnitPrice > 0 && Product?.measurePerSaleUnit) {
+      setMeasurePrice(
+        getMeasurePrice(Product?.measurePerSaleUnit, Product?.saleUnitPrice)
+      );
+    }
   }, [Product?.saleUnitPrice, Product?.measurePerSaleUnit]);
 
   return (
@@ -132,6 +153,7 @@ const SaleDataTab = ({
             min={0}
             max={100}
             step={1}
+            value={[Rentabilidad]}
             onValueChange={(value) => setRentabilidad(value[0])}
           />
         </div>
@@ -187,29 +209,84 @@ const SaleDataTab = ({
             }
           />
         </div>
-        <div className="row-start-3 row-span-5 col-start-1 col-span-6 rounded flex flex-col ">
-          <h2 className="text-2xl font-semibold mb-4">Información de venta</h2>
-          <div className="grid grid-cols-10 grid-rows-5 gap-2 h-full">
-            <Card className="bg-primary-foreground row-start-1 col-start-1 col-span-2 row-span-2">
+        <div className="row-start-3 row-span-2 col-start-1 col-span-6 flex flex-col justify-center items-center">
+          <Label className="text-lg">
+            Descuento por {Product?.saleUnit}: % {Product?.discountPercentage}
+          </Label>
+          <Slider
+            className="w-1/3 my-4"
+            min={0}
+            max={100}
+            step={1}
+            value={[Product?.discountPercentage]}
+            onValueChange={(value) =>
+              setProduct((prev) => ({ ...prev, discountPercentage: value[0] }))
+            }
+          />
+        </div>
+        <div className="row-start-5 row-span-3 col-span-6 rounded flex flex-col justify-start items-center">
+          <h2 className="text-2xl font-semibold mb-4 text-center">
+            Información de venta
+          </h2>
+          <div className="flex flex-row gap-4">
+            <Card className="bg-primary-foreground">
               <CardHeader>
                 <CardTitle className="text-center">
                   Precio por {Product?.saleUnit}
                 </CardTitle>
               </CardHeader>
-              <CardContent className="text-center">
-                <span className="text-2xl">$ {Product?.saleUnitPrice}</span>
+              <CardContent className="text-center flex flex-row gap-2 justify-center items-center">
+                <span
+                  className={
+                    Product?.discountPercentage == 0
+                      ? "text-3xl"
+                      : "text-xl line-through tetx-muted-foreground"
+                  }
+                >
+                  $ {Product?.saleUnitPrice}
+                </span>
+                {Product?.discountPercentage > 0 && (
+                  <span className="text-3xl text-destructive overflow-hidden">
+                    $
+                    {Math.round(
+                      (1 - Product?.discountPercentage / 100.0) *
+                        Product?.saleUnitPrice
+                    )}
+                  </span>
+                )}
               </CardContent>
             </Card>
-            <Card className="bg-primary-foreground row-start-1 col-start-3 col-span-2 row-span-2">
-              <CardHeader>
-                <CardTitle className="text-center">
-                  Precio por {Product?.measureType}
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="text-center">
-                <span className="text-2xl">$ {MeasurePrice}</span>
-              </CardContent>
-            </Card>
+            {Product?.saleUnit !== Product?.measureType && (
+              <Card className="bg-primary-foreground">
+                <CardHeader>
+                  <CardTitle className="text-center">
+                    Precio por {Product?.measureType}
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="text-center flex flex-row gap-2 justify-center items-center">
+                  <span
+                    className={
+                      Product?.discountPercentage == 0
+                        ? "text-3xl"
+                        : "text-xl line-through tetx-muted-foreground"
+                    }
+                  >
+                    $
+                    {Product?.saleUnit && Product?.measurePerSaleUnit
+                      ? MeasurePrice
+                      : 0}
+                  </span>
+                  {Product?.discountPercentage > 0 && (
+                    <span className="text-3xl text-destructive overflow-hidden">
+                      $
+                      {Math.round(
+                        (1 - Product?.discountPercentage / 100.0) * MeasurePrice
+                      )}
+                    </span>
+                  )}
+                </CardContent>
+              </Card>
+            )}
           </div>
         </div>
         <div className="row-start-8 col-span-2 col-start-3 flex flex-row justify-between items-center gap-2">
