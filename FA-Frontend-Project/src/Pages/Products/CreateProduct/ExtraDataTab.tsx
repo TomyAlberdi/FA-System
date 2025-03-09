80b4b4142f6f4a22ae570da92aa376ed73388f26
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-03-09 02:10:03 -0300

    Add image upload and preview functionality to ExtraDataTab

--- FA-Frontend-Project/src/Pages/Products/CreateProduct/ExtraDataTab.tsx
+++ FA-Frontend-Project/src/Pages/Products/CreateProduct/ExtraDataTab.tsx
@@ -1,7 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { TabsContent } from "@/components/ui/tabs";
+import { ToastAction } from "@/components/ui/toast";
+import { useSalesContext } from "@/Context/UseSalesContext";
 import { CreateProductDTO } from "@/hooks/CatalogInterfaces";
-import { CheckCircle2, ChevronLeft, ChevronRight } from "lucide-react";
+import { useToast } from "@/hooks/use-toast";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+import { CheckCircle2, ChevronLeft, ChevronRight, Trash2 } from "lucide-react";
+import { useEffect, useState } from "react";
 
 interface ExtraDataTabProps {
   onPrevious: () => void;
@@ -14,10 +21,149 @@ const ExtraDataTab = ({
   Product,
   setProduct,
 }: ExtraDataTabProps) => {
+  const { BASE_URL } = useSalesContext();
+  const { getToken } = useKindeAuth();
+  const { toast } = useToast();
+
+  const toastDefaultError = () => {
+    toast({
+      variant: "destructive",
+      title: `Error`,
+      description: `Ocurrió un error al crear el producto.`,
+    });
+  };
+
+  //#green Image uploading and preview logic
+  const [SelectedFiles, setSelectedFiles] = useState<Array<File>>([]);
+  const [PreviewUrls, setPreviewUrls] = useState<Array<string>>([]);
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const files = Array.from(event.target.files);
+      setSelectedFiles(files);
+      // Create preview URLs
+      const urls = files.map((file) => URL.createObjectURL(file));
+      setPreviewUrls(urls);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      PreviewUrls.forEach((url) => URL.revokeObjectURL(url));
+    };
+  }, [PreviewUrls]);
+
+  const uploadImages = async (files: Array<File>) => {
+    try {
+      if (!getToken) {
+        console.error("Token is undefined");
+        toastDefaultError();
+        return;
+      }
+      const uploadPromises = files.map(async (file) => {
+        const accessToken = await getToken();
+        const responseUrl = await fetch(
+          `${BASE_URL}/img?fileName=${file.name}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        if (!responseUrl.ok) {
+          throw new Error(
+            `Failed to get presigned URL: ${responseUrl.statusText}`
+          );
+        }
+        const uploadUrl = await responseUrl.text();
+        await fetch(uploadUrl, {
+          method: "PUT",
+          body: file,
+          headers: {
+            "Content-Type": file.type,
+          },
+        });
+        return uploadUrl.split("?")[0];
+      });
+      const uploadedUrls = await Promise.all(uploadPromises);
+      setProduct((prev) => ({
+        ...prev,
+        images: [...(prev.images || []), ...uploadedUrls],
+      }));
+    } catch (error) {
+      console.error("Error during Image Upload: ", error);
+      toastDefaultError();
+    }
+  };
+  //#
+
+  //#orange Remove existing product images and preview upload images
+  const removeImage = (url: string) => {
+    setProduct((prev) => ({
+      ...prev,
+      images: prev.images.filter((image: string) => image !== url),
+    }));
+  };
+  const removePreviewImage = (url: string) => {
+    const index = PreviewUrls.indexOf(url);
+    if (index !== -1) {
+      URL.revokeObjectURL(url);
+      setPreviewUrls((prev) => prev.filter((_, i) => i !== index));
+      setSelectedFiles((prev) => prev.filter((_, i) => i !== index));
+    }
+  };
+  //#
+
   return (
     <TabsContent value="extraData" className="h-full w-full">
       <div className="h-full w-full grid grid-cols-6 grid-rows-8 gap-4">
-        
+        <div className="row-start-1 row-end-8 col-start-1 col-end-4 flex flex-col justify-start">
+          <section className="flex flex-col gap-4">
+            <Label className="text-md">Imágenes (Opcional)</Label>
+            <Input
+              type="file"
+              multiple
+              onChange={handleFileUpload}
+              accept="image/*"
+            />
+          </section>
+          <section className="pt-4 flex flex-row gap-2 flex-wrap">
+            {Product?.images?.map((image: string, index: number) => (
+              <article
+                key={index}
+                className="w-40 h-40 rounded-md border border-input bg-contain bg-no-repeat bg-center relative"
+                style={{ backgroundImage: `url(${image})` }}
+              >
+                <span
+                  className="p-2 absolute right-1 bottom-1 bg-destructive rounded-md cursor-pointer hover:opacity-85"
+                  onClick={() => removeImage(image)}
+                >
+                  <Trash2 color="white" />
+                </span>
+              </article>
+            ))}
+            {PreviewUrls.map((url, index) => (
+              <article
+                key={index}
+                className="w-40 h-40 rounded-md border border-input bg-contain bg-no-repeat bg-center relative"
+                style={{ backgroundImage: `url(${url})` }}
+              >
+                <span
+                  className="p-2 absolute right-1 bottom-1 bg-destructive rounded-md cursor-pointer hover:opacity-85"
+                  onClick={() => removePreviewImage(url)}
+                >
+                  <Trash2 color="white" />
+                </span>
+              </article>
+            ))}
+          </section>
+        </div>
+        <div className="row-start-1 row-end-8 col-start-4 col-end-7 flex flex-col justify-start">
+          <section className="flex flex-col gap-4">
+            <Label className="text-md">Características (Opcional)</Label>
+          </section>
+        </div>
         <div className="row-start-8 col-span-2 col-start-3 flex flex-row justify-between items-center gap-2">
           <Button onClick={onPrevious} className="gap-2 w-1/2">
             <ChevronLeft size={16} />
