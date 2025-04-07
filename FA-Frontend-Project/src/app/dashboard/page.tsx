51ca47af7c8e6ccd130358e60000a0a91ba0d2ea
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-04-07 16:15:33 -0300

    Mobile header & sidebar trigger

--- FA-Frontend-Project/src/app/dashboard/page.tsx
+++ FA-Frontend-Project/src/app/dashboard/page.tsx
@@ -1,32 +1,36 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import {
   SidebarInset,
-  SidebarProvider
+  SidebarProvider,
+  SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { Outlet, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { ChevronLeft } from "lucide-react";
 import { BreadcrumbsHeader } from "@/components/BreadcrumbsHeader";
+import { Separator } from "@/components/ui/separator";
 
 export default function Page() {
-
   const navigate = useNavigate();
 
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center justify-between gap-2">
+        <header className="flex h-16 shrink-0 items-center justify-start md:justify-between gap-2">
+          <div className="md:hidden px-4">
+            <SidebarTrigger />
+          </div>
+          <Separator orientation="vertical" className="md:hidden h-3/5" />
           <div className="flex items-center gap-2 px-4">
             <BreadcrumbsHeader />
           </div>
-          <div className="backButtonContainer flex items-center justify-center px-4">
+          <div className="backButtonContainer flex items-center justify-center px-4 ml-auto md:ml-0">
             <Button
-              className="bg-primary rounded-md text-lg w-full"
+              className="bg-primary rounded-md w-full"
               onClick={() => navigate(-1)}
             >
-              <ChevronLeft size={30} />
-              Volver
+              <ChevronLeft className="large-icon" />
             </Button>
           </div>
         </header>
